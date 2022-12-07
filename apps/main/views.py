c9be3b3a9e7ebb2619d01d857fc845da4b5e6d1a
inmate37 <x.public.profile@gmail.com>
2022-12-07 18:44:43 +0600

    Fast commit

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# Django
+from django.http.request import HttpRequest
+from django.shortcuts import (
+    HttpResponse,
+    render
+)
 
-# Create your views here.
+
+def simple(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(
+        '<h1>Что нибудь</h1>'
+    )
+
+
+def index(request: HttpRequest) -> HttpRequest:
+    """index view."""
+
+    numbers: list[int] = []
+    i: int
+    for i in range(1, 11):
+        numbers.append(i)
+
+    ctx_data: dict[str, str | list[int]] = {
+        'ctx_title': 'Заголовок главной страницы',
+        'ctx_numbers': numbers
+    }
+    return render(
+        request,
+        template_name='main/index.html',
+        context=ctx_data
+    )
--- settings/base.py
+++ settings/base.py
@@ -3,26 +3,44 @@ import os
 import sys
 from pathlib import Path
 
+# Third party
+from decouple import config
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
-SECRET_KEY = 'django-insecure-8ph^-aa!cx7^8#ng*abszh!&0tod&2lois-!d%&ylcmcg+&9-='  # noqa
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = config('SECRET_KEY', cast=str)
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = ['*']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'settings.wsgi.application'
 ROOT_URLCONF = 'settings.urls'
 
-INSTALLED_APPS = [
+AUTH_USER_MODEL = 'auths.Client'
+
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+    'debug_toolbar'
 ]
+PROJECT_APPS = [
+    'abstracts.apps.AbstractsConfig',
+    'auths.apps.AuthsConfig',
+    'main.apps.MainConfig'
+]
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -31,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 TEMPLATES = [
     {
@@ -43,9 +62,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+            ]
+        }
+    }
 ]
 DATABASES = {
     'default': {
@@ -53,9 +72,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},  # noqa
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},  # noqa
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},  # noqa
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}  # noqa
 ]
@@ -63,5 +84,44 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ------------------------------------------------
+# Debug-toolbar
+#
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+# ------------------------------------------------
+# Shell-plus
+#
+SHELL_PLUS_PRE_IMPORTS = [
+    ('django.db', ('connection', 'connections', 'reset_queries')),
+    ('datetime', ('datetime', 'timedelta', 'date')),
+    ('json', ('loads', 'dumps'))
+]
+SHELL_PLUS_MODEL_ALIASES = {
+    'main': {
+        'Team': 'T',
+        'Player': 'P',
+        'Event': 'E'
+    }
+}
+SHELL_PLUS = 'ipython'
+SHELL_PLUS_PRINT_SQL = False
+SHELL_PLUS_PRINT_SQL_TRUNCATE = 1000
